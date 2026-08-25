3acf6123071c50e86ce9c1e21c94800058c4a270
Thiago Silva <thiagosilvag31@gmail.com>
2026-08-24 21:54:56 -0300

    Alterações nas Interfaces, Service e Repository.

--- MinhaApi/Program.cs
+++ MinhaApi/Program.cs
@@ -1,3 +1,6 @@
+using MinhaApi.Repositories;
+using MinhaApi.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +8,12 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
+
+//registra o Repository
+builder.Services.AddScoped<
+    IProdutoService,
+    ProdutoService>();
+    
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
