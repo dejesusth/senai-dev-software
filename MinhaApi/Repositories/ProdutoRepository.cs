d9e402ae80f35d7d975a05774c9b10aa7c522670
Thiago de Jesus Silva <thiagosilvag31@gmail.com>
2026-08-31 21:25:10 -0300

    Adiciona MySqlConnector e implementa método GetAll no ProdutoRepository

--- MinhaApi/Repositories/ProdutoRepository.cs
+++ MinhaApi/Repositories/ProdutoRepository.cs
@@ -1,17 +1,37 @@
 using MinhaApi.Models;
+using MySqlConnector;
 namespace MinhaApi.Repositories;
+public class ProdutoRepository : IProdutoRepository{
+private readonly string _connectionString;
+public ProdutoRepository(IConfiguration config) 
+=> _connectionString = config.GetConnectionString("DefaultConnection")!;
 
-public class ProdutoRepository : IProdutoRepository
-{
     private static List<Produto> _db = new()
     {
         new Produto { Id=1, Nome="Notebook", Preco=2500m, Estoque=10 },
         new Produto { Id=2, Nome="Mouse", Preco=89.90m, Estoque=50 }
     
     };
+      public IEnumerable<Produto> GetAll() {
+      var lista = new List<Produto>();
+      using var conn = new MySqlConnection(_connectionString);
+      conn.Open();
 
-    public IEnumerable<Produto> GetAll() => _db;
+      string sql = "SELECT id, nome, preco, estoque, ativo FROM produtos";
+      using var cmd = new MySqlCommand(sql, conn);
+      using var reader = cmd.ExecuteReader();
 
+      while (reader.Read()) {
+          lista.Add(new Produto {
+              Id = reader.GetInt32("id"),
+              Nome = reader.GetString("nome"),
+              Preco = reader.GetDecimal("preco"),
+              Estoque = reader.GetInt32("estoque"),
+              Ativo = reader.GetBoolean("ativo")
+          });
+      }
+      return lista;
+  }
     public Produto? GetById(int id) => _db.FirstOrDefault(p => p.Id == id);
 
     public void Add(Produto p)
